9b5d32429cbd32dc51d48e45d5a16d55dc774a60
smcarlson192 <smcarlson192@gmail.com>
2024-01-22 20:38:29 -0500

    copy from old repo

---
+++ js/index.js
@@ -0,0 +1,33 @@
+console.log("Your index.js file is loaded correctly!");
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+                                
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("on");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
